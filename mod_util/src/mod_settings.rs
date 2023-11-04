f8e2c506b1de4f6a9696cec365bbb70cb89eee57
Florian <me@fgardt.dev>
2023-11-04 18:34:03 +0100

    feat: serializing and deserializing to mod-settings.dat format

--- mod_util/src/mod_settings.rs
+++ mod_util/src/mod_settings.rs
@@ -5,6 +5,8 @@ use serde_with::skip_serializing_none;
 
 use serde_helper as helper;
 
+use crate::property_tree::PropertyTree;
+
 #[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CommonSettingsData {
@@ -135,4 +137,89 @@ pub struct ModSettings {
         skip_serializing_if = "HashMap::is_empty"
     )]
     pub color_settings: HashMap<String, ColorSetting>,
+}
+
+#[derive(Debug, Clone)]
+pub struct SettingsDat {
+    pub version: u64, // https://wiki.factorio.com/Version_string_format
+
+    pub startup: HashMap<String, PropertyTree>,
+    pub runtime_global: HashMap<String, PropertyTree>,
+    pub runtime_per_user: HashMap<String, PropertyTree>,
+}
+
+impl SettingsDat {
+    #[must_use]
+    pub fn from_bytes(bytes: &[u8]) -> Option<Self> {
+        let version_buff = [
+            *bytes.first()?,
+            *bytes.get(1)?,
+            *bytes.get(2)?,
+            *bytes.get(3)?,
+            *bytes.get(4)?,
+            *bytes.get(5)?,
+            *bytes.get(6)?,
+            *bytes.get(7)?,
+        ];
+        let version = u64::from_le_bytes(version_buff);
+        //let false_bool: bool = *bytes.get(8)? == 0; // always false bool
+
+        let data = PropertyTree::from_bytes(&bytes[9..])?;
+
+        let PropertyTree::Dictionary(data) = data else {
+            return None;
+        };
+
+        let PropertyTree::Dictionary(startup) = data.get("startup")?.clone() else {
+            return None;
+        };
+
+        let PropertyTree::Dictionary(runtime_global) = data.get("runtime-global")?.clone() else {
+            return None;
+        };
+
+        let PropertyTree::Dictionary(runtime_per_user) = data.get("runtime-per-user")?.clone()
+        else {
+            return None;
+        };
+
+        Some(Self {
+            version,
+            startup,
+            runtime_global,
+            runtime_per_user,
+        })
+    }
+
+    #[must_use]
+    pub fn to_bytes(&self) -> Vec<u8> {
+        let mut bytes = Vec::new();
+
+        bytes.extend_from_slice(&self.version.to_le_bytes());
+        bytes.push(0); // always false bool
+
+        let data = PropertyTree::Dictionary(
+            vec![
+                (
+                    "startup".to_owned(),
+                    PropertyTree::Dictionary(self.startup.clone()),
+                ),
+                (
+                    "runtime-global".to_owned(),
+                    PropertyTree::Dictionary(self.runtime_global.clone()),
+                ),
+                (
+                    "runtime-per-user".to_owned(),
+                    PropertyTree::Dictionary(self.runtime_per_user.clone()),
+                ),
+            ]
+            .iter()
+            .cloned()
+            .collect(),
+        );
+
+        bytes.extend(data.to_bytes());
+
+        bytes
+    }
 }
