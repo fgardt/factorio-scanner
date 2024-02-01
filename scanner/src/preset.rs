750ecca7a78f1cff6e97a192a4455a7a03215842
Florian Gebhardt <me@fgardt.dev>
2024-02-01 14:39:01 +0100

    feat: more presets (#77)

--- scanner/src/preset.rs
+++ scanner/src/preset.rs
@@ -1,26 +1,46 @@
 use clap::builder::PossibleValue;
 use mod_util::{mod_info::Version, UsedVersions};
-use strum::EnumIter;
+use strum::{EnumIter, VariantArray};
 
 #[allow(clippy::module_name_repetitions)]
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, EnumIter)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, EnumIter, VariantArray)]
 pub enum Preset {
     K2,
     SE,
     K2SE,
+    IR3,
+    PyAE,
+    FF,
+    FFK2,
+    Nullius,
     SeaBlock,
+    ExoticIndustries,
+    Ultracube,
 }
 
 impl Preset {
     pub fn used_mods(self) -> UsedVersions {
         match self {
             Self::K2 => vec![("Krastorio2".to_owned(), Version::new(1, 3, 23))],
-            Self::SE => vec![("space-exploration".to_owned(), Version::new(0, 6, 119))],
+            Self::SE => vec![("space-exploration".to_owned(), Version::new(0, 6, 123))],
             Self::K2SE => vec![
                 ("Krastorio2".to_owned(), Version::new(1, 3, 23)),
-                ("space-exploration".to_owned(), Version::new(0, 6, 119)),
+                ("space-exploration".to_owned(), Version::new(0, 6, 123)),
             ],
+            Self::IR3 => vec![("IndustrialRevolution3".to_owned(), Version::new(3, 1, 20))],
+            Self::PyAE => vec![("pyalternativeenergy".to_owned(), Version::new(1, 2, 17))],
+            Self::FF => vec![("FreightForwardingPack".to_owned(), Version::new(1, 2, 1))],
+            Self::FFK2 => vec![
+                ("FreightForwardingPack".to_owned(), Version::new(1, 2, 1)),
+                ("Krastorio2".to_owned(), Version::new(1, 3, 23)),
+            ],
+            Self::Nullius => vec![("nullius".to_owned(), Version::new(1, 9, 1))],
             Self::SeaBlock => vec![("SeaBlockMetaPack".to_owned(), Version::new(1, 1, 4))],
+            Self::ExoticIndustries => vec![(
+                "exotic-industries-modpack".to_owned(),
+                Version::new(0, 5, 2),
+            )],
+            Self::Ultracube => vec![("Ultracube".to_owned(), Version::new(0, 4, 4))],
         }
         .iter()
         .cloned()
@@ -32,13 +52,7 @@ impl Preset {
 
 impl ToString for Preset {
     fn to_string(&self) -> String {
-        match self {
-            Self::K2 => "K2",
-            Self::SE => "SE",
-            Self::K2SE => "K2SE",
-            Self::SeaBlock => "SeaBlock",
-        }
-        .to_owned()
+        format!("{self:?}")
     }
 }
 
@@ -50,7 +64,14 @@ impl std::str::FromStr for Preset {
             "k2" => Ok(Self::K2),
             "se" => Ok(Self::SE),
             "k2se" | "k2+se" | "sek2" | "se+k2" => Ok(Self::K2SE),
+            "ir3" => Ok(Self::IR3),
+            "pyae" | "pyanodons" => Ok(Self::PyAE),
+            "ff" | "freightforwarding" => Ok(Self::FF),
+            "ffk2" => Ok(Self::FFK2),
+            "nullius" => Ok(Self::Nullius),
             "seablock" | "sb" => Ok(Self::SeaBlock),
+            "ei" | "exoticindustries" => Ok(Self::ExoticIndustries),
+            "ultracube" => Ok(Self::Ultracube),
             _ => Err(format!("unknown preset: {s}")),
         }
     }
@@ -58,7 +79,7 @@ impl std::str::FromStr for Preset {
 
 impl clap::ValueEnum for Preset {
     fn value_variants<'a>() -> &'a [Self] {
-        &[Self::K2, Self::SE, Self::K2SE, Self::SeaBlock]
+        Self::VARIANTS
     }
 
     fn to_possible_value(&self) -> Option<PossibleValue> {
@@ -69,9 +90,18 @@ impl clap::ValueEnum for Preset {
                 PossibleValue::new("K2SE")
                     .aliases(["k2se", "K2+SE", "k2+se", "SEK2", "sek2", "SE+K2", "se+k2"]),
             ),
+            Self::IR3 => Some(PossibleValue::new("IR3").alias("ir3")),
+            Self::PyAE => Some(PossibleValue::new("PyAE").aliases(["pyae", "pyanodons"])),
+            Self::FF => Some(PossibleValue::new("FF").aliases(["ff", "FreightForwarding"])),
+            Self::FFK2 => Some(PossibleValue::new("FFK2").aliases(["ffk2", "FF+K2", "ff+k2"])),
+            Self::Nullius => Some(PossibleValue::new("Nullius").alias("nullius")),
             Self::SeaBlock => {
                 Some(PossibleValue::new("SeaBlock").aliases(["seablock", "SB", "sb"]))
             }
+            Self::ExoticIndustries => {
+                Some(PossibleValue::new("EI").aliases(["ei", "exoticindustries"]))
+            }
+            Self::Ultracube => Some(PossibleValue::new("Ultracube").alias("ultracube")),
         }
     }
 }
